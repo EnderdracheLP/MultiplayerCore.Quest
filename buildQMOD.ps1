ad7be5d81c22df3ce78007ce4ee5ed3658881e95
EnderdracheLP <enderdrachelp@gmail.com>
2022-03-28 15:41:38 +0200

    Forgot build scripts

---
+++ buildQMOD.ps1
@@ -0,0 +1,102 @@
+Param(
+    [Parameter(Mandatory=$false, HelpMessage="The name the output qmod file should have")][String] $qmodname="MultiplayerCore",
+
+    [Parameter(Mandatory=$false, HelpMessage="Switch to create a clean compilation")]
+    [Alias("rebuild")]
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false, HelpMessage="Prints the help instructions")]
+    [Switch] $help,
+
+    [Parameter(Mandatory=$false, HelpMessage="Tells the script to not compile and only package the existing files")]
+    [Alias("actions", "pack")]
+    [Switch] $package
+)
+
+# Builds a .qmod file for loading with QP or BMBF
+
+
+if ($help -eq $true) {
+    echo "`"BuildQmod <qmodName>`" - Copiles your mod into a `".so`" or a `".a`" library"
+    echo "`n-- Parameters --`n"
+    echo "qmodName `t The file name of your qmod"
+
+    echo "`n-- Arguments --`n"
+
+    echo "-clean `t`t Performs a clean build on both your library and the qmod"
+    echo "-help `t`t Prints this"
+    echo "-package `t Only packages existing files, without recompiling`n"
+
+    exit
+}
+
+if ($qmodName -eq "")
+{
+    echo "Give a proper qmod name and try again"
+    exit
+}
+
+if ($package -eq $true) {
+    $qmodName = "$($env:module_id)_$($env:version)"
+    echo "Actions: Packaging QMod $qmodName"
+}
+if (($args.Count -eq 0) -And $package -eq $false) {
+echo "Creating QMod $qmodName"
+    & $PSScriptRoot/build.ps1 -clean:$clean
+
+    if ($LASTEXITCODE -ne 0) {
+        echo "Failed to build, exiting..."
+        exit $LASTEXITCODE
+    }
+
+    qpm-rust qmod build
+}
+
+echo "Creating qmod from mod.json"
+
+$mod = "./mod.json"
+$modJson = Get-Content $mod -Raw | ConvertFrom-Json
+
+$filelist = @($mod)
+
+$cover = "./" + $modJson.coverImage
+if ((-not ($cover -eq "./")) -and (Test-Path $cover))
+{ 
+    $filelist += ,$cover
+} else {
+    echo "No cover Image found"
+}
+
+foreach ($mod in $modJson.modFiles)
+{
+    $path = "./build/" + $mod
+    if (-not (Test-Path $path))
+    {
+        $path = "./extern/libs/" + $mod
+    }
+    $filelist += $path
+}
+
+foreach ($lib in $modJson.libraryFiles)
+{
+    $path = "./extern/libs/" + $lib
+    if (-not (Test-Path $path))
+    {
+        $path = "./build/" + $lib
+    }
+    $filelist += $path
+}
+
+$zip = $qmodName + ".zip"
+$qmod = $qmodName + ".qmod"
+
+if ((-not ($clean.IsPresent)) -and (Test-Path $qmod))
+{
+    echo "Making Clean Qmod"
+    Move-Item $qmod $zip -Force
+}
+
+Compress-Archive -Path $filelist -DestinationPath $zip -Update
+Move-Item $zip $qmod -Force
+
+echo "Task Completed"
