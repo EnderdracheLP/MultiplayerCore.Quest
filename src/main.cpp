1bb2112c18b4d925b1412eed4680e4697f85c037
cubic <79692655+cubicgraphics@users.noreply.github.com>
2022-04-21 13:32:09 +0100

    and that gone

--- src/main.cpp
+++ src/main.cpp
@@ -455,7 +455,6 @@ MAKE_HOOK_MATCH(MultiplayerLevelLoader_LoadLevel, &MultiplayerLevelLoader::LoadL
                                 }
                                 );
                         }
-                //send entitlement packet from here?
                     }
                 );
             }
