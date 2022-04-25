04e2d2cf09e3fc5b4fbeb97a2f20f92d4408e7f9
EnderdracheLP <enderdrachelp@gmail.com>
2022-04-25 20:10:38 +0200

    Box enum value directly

--- shared/CodegenExtensions/EnumUtils.hpp
+++ shared/CodegenExtensions/EnumUtils.hpp
@@ -15,11 +15,12 @@ namespace MultiplayerCore {
         static std::string GetEnumName(T enumValue, bool ignoreCase) {
         static_assert(std::is_convertible_v<T, int>);
             std::string enumName = "";
-            int enumInt = static_cast<int>(enumValue);
+            // int enumInt = static_cast<int>(enumValue);
             Il2CppClass* enumClass = classof(T);
             Il2CppReflectionType* enumType = enumClass ? ::il2cpp_utils::GetSystemType(enumClass) : nullptr;
-            if (enumType != nullptr && System::Enum::IsDefined(enumType, il2cpp_functions::value_box(classof(T), &enumInt))) {
-                enumName = static_cast<std::string>(System::Enum::GetName(enumType, il2cpp_functions::value_box(classof(T), &enumInt)));
+            Il2CppObject* boxedValue = il2cpp_functions::value_box(classof(T), &enumValue.value);
+            if (enumType != nullptr && System::Enum::IsDefined(enumType, boxedValue)) {
+                enumName = static_cast<std::string>(System::Enum::GetName(enumType, boxedValue));
             }
             else {
                 enumName = "";
