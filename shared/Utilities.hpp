dc5c9e81f8e1ad55464f3612dd3b7a5ef8e55e61
EnderdracheLP <enderdrachelp@gmail.com>
2022-05-26 19:54:52 +0200

    Add ClearDelegate to Utilities, make Utilities struct instead of class

--- shared/Utilities.hpp
+++ shared/Utilities.hpp
@@ -3,14 +3,17 @@
 #include "songloader/shared/API.hpp"
 
 namespace MultiplayerCore {
-    class Utilities {
-    public:
+    struct Utilities {
         static inline std::string GetHash(const std::string& levelId) {
             return levelId.substr(RuntimeSongLoader::API::GetCustomLevelsPrefix().length(), levelId.length() - RuntimeSongLoader::API::GetCustomLevelsPrefix().length());
         }
 
         static inline StringW HashForLevelID(StringW levelId) {
             return levelId->StartsWith(RuntimeSongLoader::API::GetCustomLevelsPrefix().c_str()) ? levelId->Substring(RuntimeSongLoader::API::GetCustomLevelsPrefix().length()) : StringW();
+        }
+
+        static inline void ClearDelegate(Il2CppDelegate* delegate) {
+            il2cpp_utils::ClearDelegate({((MethodInfo*)(void*)(delegate)->method)->methodPointer, true});
         }
     };
 }
