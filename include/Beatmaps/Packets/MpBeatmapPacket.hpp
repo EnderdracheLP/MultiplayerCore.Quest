edd6e06671a004df130d9df06123bd908ab0add4
EnderdracheLP <enderdrachelp@gmail.com>
2022-07-20 21:33:59 +0200

    Deserialize new data, currently unused

--- include/Beatmaps/Packets/MpBeatmapPacket.hpp
+++ include/Beatmaps/Packets/MpBeatmapPacket.hpp
@@ -18,11 +18,13 @@ DECLARE_CLASS_CUSTOM_DLL(MultiplayerCore::Beatmaps::Packets, MpBeatmapPacket, Mu
     DECLARE_INSTANCE_FIELD(StringW, characteristic);
     DECLARE_INSTANCE_FIELD(GlobalNamespace::BeatmapDifficulty, difficulty);
 
-    DECLARE_CTOR(New);
+    DECLARE_DEFAULT_CTOR();
 
     DECLARE_OVERRIDE_METHOD(void, Serialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Serialize>::get(), LiteNetLib::Utils::NetDataWriter* writer);
     DECLARE_OVERRIDE_METHOD(void, Deserialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Deserialize>::get(), LiteNetLib::Utils::NetDataReader* reader);
 
     public:
         static MpBeatmapPacket* CS_Ctor(GlobalNamespace::PreviewDifficultyBeatmap* beatmap);
+
+        std::unordered_map<uint8_t, std::vector<StringW>> requirements;
 )
