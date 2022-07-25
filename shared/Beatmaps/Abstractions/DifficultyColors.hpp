082d55b95fa996d726af880568736d001d1740f8
EnderdracheLP <enderdrachelp@gmail.com>
2022-07-25 20:50:13 +0200

    Add DifficultyColors

---
+++ shared/Beatmaps/Abstractions/DifficultyColors.hpp
@@ -0,0 +1,99 @@
+#pragma once
+#include "custom-types/shared/macros.hpp"
+#include "Networking/Abstractions/MpPacket.hpp"
+#include "UnityEngine/Color.hpp"
+
+// DECLARE_CLASS_CUSTOM_DLL(MultiplayerCore::Beatmaps::Abstractions, DifficultyColors, MultiplayerCore::Networking::Abstractions::MpPacket, "MultiplayerCore.Beatmaps.Abstractions",
+//     DECLARE_DEFAULT_CTOR();
+
+//     DECLARE_OVERRIDE_METHOD(void, Serialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Serialize>::get(), LiteNetLib::Utils::NetDataWriter* writer);
+//     DECLARE_OVERRIDE_METHOD(void, Deserialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Deserialize>::get(), LiteNetLib::Utils::NetDataReader* reader);
+
+//     public:
+//         UnityEngine::Color _colorLeft;
+//         UnityEngine::Color _colorRight;
+//         UnityEngine::Color _envColorLeft;
+//         UnityEngine::Color _envColorRight;
+//         UnityEngine::Color _envColorLeftBoost;
+//         UnityEngine::Color _envColorRightBoost;
+//         UnityEngine::Color _obstacleColor;
+
+//         template<::il2cpp_utils::CreationType creationType = ::il2cpp_utils::CreationType::Temporary>
+//         static inline DifficultyColors* New_ctor(UnityEngine::Color colorLeft, UnityEngine::Color colorRight, 
+//             UnityEngine::Color envColorLeft, UnityEngine::Color envColorRight, 
+//             UnityEngine::Color envColorLeftBoost, UnityEngine::Color envColorRightBoost,
+//             UnityEngine::Color obstacleColor)
+//         {
+//             DifficultyColors* instance = MultiplayerCore::Beatmaps::Abstractions::DifficultyColors::New_ctor<creationType>();
+//             instance->colorLeft = colorLeft;
+//             instance->colorRight = colorRight;
+//             instance->envColorLeft = envColorLeft;
+//             instance->envColorRight = envColorRight;
+//             instance->envColorLeftBoost = envColorLeftBoost;
+//             instance->envColorRightBoost = envColorRightBoost;
+//             instance->obstacleColor = obstacleColor;
+//             return instance;
+//         }
+
+//         static inline void SerializeMapColor(LiteNetLib::Utils::NetDataWriter* writer, UnityEngine::Color color)
+//         {
+//             writer->Put(color.r);
+//             writer->Put(color.g);
+//             writer->Put(color.b);
+//         }
+
+//         static inline UnityEngine::Color DeserializeMapColor(LiteNetLib::Utils::NetDataReader* reader)
+//         {
+//             UnityEngine::Color color;
+//             color.r = reader->GetFloat();
+//             color.g = reader->GetFloat();
+//             color.b = reader->GetFloat();
+//         }
+// )
+
+namespace MultiplayerCore::Beatmaps::Abstractions {
+    struct DifficultyColors {
+        
+        UnityEngine::Color _colorLeft;
+        UnityEngine::Color _colorRight;
+        UnityEngine::Color _envColorLeft;
+        UnityEngine::Color _envColorRight;
+        UnityEngine::Color _envColorLeftBoost;
+        UnityEngine::Color _envColorRightBoost;
+        UnityEngine::Color _obstacleColor;
+
+        void Serialize(LiteNetLib::Utils::NetDataWriter* writer);
+        void Deserialize(LiteNetLib::Utils::NetDataReader* reader);
+
+        constexpr DifficultyColors() {}
+        constexpr DifficultyColors(UnityEngine::Color colorLeft, UnityEngine::Color colorRight, 
+            UnityEngine::Color envColorLeft, UnityEngine::Color envColorRight, 
+            UnityEngine::Color envColorLeftBoost, UnityEngine::Color envColorRightBoost,
+            UnityEngine::Color obstacleColor)
+        {
+            this->_colorLeft = colorLeft;
+            this->_colorRight = colorRight;
+            this->_envColorLeft = envColorLeft;
+            this->_envColorRight = envColorRight;
+            this->_envColorLeftBoost = envColorLeftBoost;
+            this->_envColorRightBoost = envColorRightBoost;
+            this->_obstacleColor = obstacleColor;
+        }
+
+        static inline void SerializeMapColor(LiteNetLib::Utils::NetDataWriter* writer, UnityEngine::Color color)
+        {
+            writer->Put(color.r);
+            writer->Put(color.g);
+            writer->Put(color.b);
+        }
+
+        static inline UnityEngine::Color DeserializeMapColor(LiteNetLib::Utils::NetDataReader* reader)
+        {
+            UnityEngine::Color color;
+            color.r = reader->GetFloat();
+            color.g = reader->GetFloat();
+            color.b = reader->GetFloat();
+            return color;
+        }
+    };
+}
---
+++ src/Beatmaps/Abstractions/DifficultyColors.cpp
@@ -0,0 +1,63 @@
+// TODO: Possibly remove the whole type as for right now I just can't use it the intended way
+
+#include "Beatmaps/Abstractions/DifficultyColors.hpp"
+
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "songdownloader/shared/BeatSaverAPI.hpp"
+#include "questui/shared/CustomTypes/Components/MainThreadScheduler.hpp"
+
+#include "System/Threading/Tasks/Task_1.hpp"
+#include "System/Threading/Tasks/TaskStatus.hpp"
+
+#include <vector>
+
+// DEFINE_TYPE(MultiplayerCore::Beatmaps::Abstractions, DifficultyColors);
+
+namespace MultiplayerCore::Beatmaps::Abstractions {
+
+	void DifficultyColors::Serialize(NetDataWriter* writer)
+	{
+		uint8_t colors = (uint8_t)(_colorLeft ? 1 : 0);
+		colors |= (uint8_t)((_colorRight ? 1 : 0) << 1);
+		colors |= (uint8_t)((_envColorLeft ? 1 : 0) << 2);
+		colors |= (uint8_t)((_envColorRight ? 1 : 0) << 3);
+		colors |= (uint8_t)((_envColorLeftBoost ? 1 : 0) << 4);
+		colors |= (uint8_t)((_envColorRightBoost ? 1 : 0) << 5);
+		colors |= (uint8_t)((_obstacleColor ? 1 : 0) << 6);
+		writer->Put(colors);
+
+		if (_colorLeft)
+			SerializeMapColor(writer, _colorLeft);
+		if (_colorRight)
+			SerializeMapColor(writer, _colorRight);
+		if (_envColorLeft)
+			SerializeMapColor(writer, _envColorLeft);
+		if (_envColorRight)
+			SerializeMapColor(writer, _envColorRight);
+		if (_envColorLeftBoost)
+			SerializeMapColor(writer, _envColorLeftBoost);
+		if (_envColorRightBoost)
+			SerializeMapColor(writer, _envColorRightBoost);
+		if (_obstacleColor)
+			SerializeMapColor(writer, _obstacleColor);
+	}
+
+	void DifficultyColors::Deserialize(NetDataReader* reader)
+	{
+		uint8_t colors = reader->GetByte();
+		if ((colors & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 1) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 2) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 3) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 4) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 5) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+		if (((colors >> 6) & 0x1) != 0)
+			_colorLeft = DeserializeMapColor(reader);
+	}
+}
