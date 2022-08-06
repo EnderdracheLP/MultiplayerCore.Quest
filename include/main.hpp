9aab7c981009d2e25ed82e524d70235f6c2a7893
EnderdracheLP <enderdrachelp@gmail.com>
2022-08-06 12:31:02 +0200

    Add CT delegate include to main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -9,6 +9,7 @@
 #include "beatsaber-hook/shared/config/config-utils.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "custom-types/shared/delegate.hpp"
 
 // Cpp includes
 #include <map>
