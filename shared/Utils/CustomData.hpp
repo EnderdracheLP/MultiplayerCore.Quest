2bdb9d52ae8db6774b4124320b1094eaa612655a
EnderdracheLP <enderdrachelp@gmail.com>
2022-08-06 12:26:05 +0200

    Remove CustomData class

--- shared/Utils/CustomData.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "GlobalNamespace/CustomPreviewBeatmapLevel.hpp"
-
-namespace MultiplayerCore::Utils {
-
-
-    bool HasRequirement(const std::optional<GlobalNamespace::CustomPreviewBeatmapLevel*>& beatmapOpt);
-}
