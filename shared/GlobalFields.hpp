426b2f69a009f68deae47bbc708bf0cdefaac20e
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-02 21:14:32 +0200

    out with the old

--- shared/GlobalFields.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GlobalNamespace/MultiplayerSessionManager.hpp"
-
-namespace MultiplayerCore {
-    // Plugin setup stuff
-    extern GlobalNamespace::MultiplayerSessionManager* _multiplayerSessionManager;
-}
