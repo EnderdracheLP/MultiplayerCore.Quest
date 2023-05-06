9e0a5324e109917cf419a3fff973bfdda8ca58fb
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-06 15:24:17 +0200

    Fix To binding

--- src/Hooks/MainSystemBinderHooks.cpp
+++ src/Hooks/MainSystemBinderHooks.cpp
@@ -1,9 +1,11 @@
 #include "logging.hpp"
 #include "hooking.hpp"
 
+#include "lapiz/shared/arrayutils.hpp"
 #include "GlobalNamespace/MainSystemInit.hpp"
 #include "Zenject/DiContainer.hpp"
 #include "Zenject/ConcreteIdBinderGeneric_1.hpp"
+#include "Zenject/FromBinderNonGeneric.hpp"
 #include "Objects/MpEntitlementChecker.hpp"
 
 MAKE_AUTO_HOOK_ORIG_MATCH(MainSystemInit_InstallBindings, &::GlobalNamespace::MainSystemInit::InstallBindings, void, GlobalNamespace::MainSystemInit* self, Zenject::DiContainer* container) {
@@ -11,5 +13,6 @@ MAKE_AUTO_HOOK_ORIG_MATCH(MainSystemInit_InstallBindings, &::GlobalNamespace::Ma
 
     // TODO: check if this actually will just work
     container->Unbind<GlobalNamespace::NetworkPlayerEntitlementChecker*>();
-    container->Bind<GlobalNamespace::NetworkPlayerEntitlementChecker*>()->To<MultiplayerCore::Objects::MpEntitlementChecker*>()->FromNewComponentOnRoot()->AsSingle()->NonLazy();
+    auto toArr = Lapiz::ArrayUtils::TypeArray<MultiplayerCore::Objects::MpEntitlementChecker*>();
+    container->Bind<GlobalNamespace::NetworkPlayerEntitlementChecker*>()->To(toArr)->FromNewComponentOnRoot()->AsSingle()->NonLazy();
 }
