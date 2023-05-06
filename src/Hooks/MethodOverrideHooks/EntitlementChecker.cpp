47f97c4e1001e484278c8f064d8e8c1ddf149bc5
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-06 20:24:02 +0200

    Invoke lock

--- src/Hooks/MethodOverrideHooks/EntitlementChecker.cpp
+++ src/Hooks/MethodOverrideHooks/EntitlementChecker.cpp
@@ -1,5 +1,6 @@
 #include "hooking.hpp"
 #include "logging.hpp"
+#include "InvokingLock.hpp"
 
 #include "GlobalNamespace/NetworkPlayerEntitlementChecker.hpp"
 #include "GlobalNamespace/EntitlementsStatus.hpp"
@@ -12,33 +13,57 @@ using namespace MultiplayerCore::Objects;
 
 // override Start
 MAKE_AUTO_HOOK_MATCH(NetworkPlayerEntitlementChecker_Start, &::GlobalNamespace::NetworkPlayerEntitlementChecker::Start, void, GlobalNamespace::NetworkPlayerEntitlementChecker* self) {
-    NetworkPlayerEntitlementChecker_Start(self);
-
-    static auto customKlass = classof(MpEntitlementChecker*);
-    if (self->klass == customKlass)
-        reinterpret_cast<MpEntitlementChecker*>(self)->Start_override();
+    INVOKE_LOCK(NetworkPlayerEntitlementChecker_Start);
+    if (!lock) {
+        NetworkPlayerEntitlementChecker_Start(self);
+    } else {
+        static auto customKlass = classof(MpEntitlementChecker*);
+        if (self->klass == customKlass)
+            reinterpret_cast<MpEntitlementChecker*>(self)->Start_override();
+        else
+            NetworkPlayerEntitlementChecker_Start(self);
+    }
 }
 // override OnDestroy
 MAKE_AUTO_HOOK_MATCH(NetworkPlayerEntitlementChecker_OnDestroy, &::GlobalNamespace::NetworkPlayerEntitlementChecker::OnDestroy, void, GlobalNamespace::NetworkPlayerEntitlementChecker* self) {
-    static auto customKlass = classof(MpEntitlementChecker*);
-    if (self->klass == customKlass)
-        reinterpret_cast<MpEntitlementChecker*>(self)->OnDestroy_override();
+    INVOKE_LOCK(NetworkPlayerEntitlementChecker_OnDestroy);
 
-    NetworkPlayerEntitlementChecker_OnDestroy(self);
+    if (!lock) {
+        NetworkPlayerEntitlementChecker_OnDestroy(self);
+    } else {
+        static auto customKlass = classof(MpEntitlementChecker*);
+        if (self->klass == customKlass)
+            reinterpret_cast<MpEntitlementChecker*>(self)->OnDestroy_override();
+        else
+            NetworkPlayerEntitlementChecker_OnDestroy(self);
+    }
 }
 // override HandleGetIsEntitledToLevel
 MAKE_AUTO_HOOK_MATCH(NetworkPlayerEntitlementChecker_HandleGetIsEntitledToLevel, &::GlobalNamespace::NetworkPlayerEntitlementChecker::HandleGetIsEntitledToLevel, void, GlobalNamespace::NetworkPlayerEntitlementChecker* self, StringW userId, StringW levelId) {
-    static auto customKlass = classof(MpEntitlementChecker*);
-    if (self->klass == customKlass)
-        reinterpret_cast<MpEntitlementChecker*>(self)->HandleGetIsEntitledToLevel_override(userId, levelId);
-    else
+    INVOKE_LOCK(NetworkPlayerEntitlementChecker_HandleGetIsEntitledToLevel);
+
+    if (!lock) {
         NetworkPlayerEntitlementChecker_HandleGetIsEntitledToLevel(self, userId, levelId);
+    } else {
+        static auto customKlass = classof(MpEntitlementChecker*);
+        if (self->klass == customKlass)
+            reinterpret_cast<MpEntitlementChecker*>(self)->HandleGetIsEntitledToLevel_override(userId, levelId);
+        else
+            NetworkPlayerEntitlementChecker_HandleGetIsEntitledToLevel(self, userId, levelId);
+    }
 }
+
 // override GetEntitlementStatus
 MAKE_AUTO_HOOK_MATCH(NetworkPlayerEntitlementChecker_GetEntitlementStatus, &::GlobalNamespace::NetworkPlayerEntitlementChecker::GetEntitlementStatus, ::System::Threading::Tasks::Task_1<::GlobalNamespace::EntitlementsStatus>*, GlobalNamespace::NetworkPlayerEntitlementChecker* self, StringW levelId) {
-    static auto customKlass = classof(MpEntitlementChecker*);
-    if (self->klass == customKlass)
-        return reinterpret_cast<MpEntitlementChecker*>(self)->GetEntitlementStatus_override(levelId);
-    else
+    INVOKE_LOCK(NetworkPlayerEntitlementChecker_GetEntitlementStatus);
+
+    if (!lock) {
         return NetworkPlayerEntitlementChecker_GetEntitlementStatus(self, levelId);
+    } else {
+        static auto customKlass = classof(MpEntitlementChecker*);
+        if (self->klass == customKlass)
+            return reinterpret_cast<MpEntitlementChecker*>(self)->GetEntitlementStatus_override(levelId);
+        else
+            return NetworkPlayerEntitlementChecker_GetEntitlementStatus(self, levelId);
+    }
 }
