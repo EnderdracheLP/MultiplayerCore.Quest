e4b1c6a0782daa00d005593accb6a49af93b8b1f
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-09 12:34:04 +0200

    Fix empty platform in lobby if not uneven

--- src/Hooks/MultiplayerLobbyPositionHooks.cpp
+++ src/Hooks/MultiplayerLobbyPositionHooks.cpp
@@ -5,9 +5,15 @@
 #include "GlobalNamespace/MultiplayerLobbyAvatarPlaceManager.hpp"
 #include "GlobalNamespace/MultiplayerLobbyCenterStageManager.hpp"
 #include "GlobalNamespace/MultiplayerPlayersManager.hpp"
+#include "GlobalNamespace/MultiplayerLobbyAvatarPlace_Pool.hpp"
+#include "GlobalNamespace/MultiplayerLobbyAvatarPlace.hpp"
 #include "GlobalNamespace/MultiplayerPlayerStartState.hpp"
 #include "GlobalNamespace/MultiplayerLobbyAvatarController.hpp"
 #include "GlobalNamespace/MultiplayerLobbyAvatarController_Factory.hpp"
+#include "GlobalNamespace/MultiplayerLocalActivePlayerFacade_Factory.hpp"
+#include "GlobalNamespace/MultiplayerLocalInactivePlayerFacade_Factory.hpp"
+#include "GlobalNamespace/MultiplayerConnectedPlayerFacade_Factory.hpp"
+#include "GlobalNamespace/MultiplayerSpectatorController.hpp"
 #include "GlobalNamespace/MultiplayerPlayerPlacement.hpp"
 #include "GlobalNamespace/MultiplayerPlayerLayout.hpp"
 #include "GlobalNamespace/MultiplayerLayoutProvider.hpp"
@@ -17,12 +23,18 @@
 #include "GlobalNamespace/ILobbyStateDataModel.hpp"
 #include "GlobalNamespace/IMultiplayerSessionManager.hpp"
 #include "GlobalNamespace/ConnectedPlayerHelpers.hpp"
+#include "GlobalNamespace/MultiplayerLevelCompletionResults.hpp"
 #include "UnityEngine/Quaternion.hpp"
 #include "UnityEngine/Transform.hpp"
 #include "System/Collections/Generic/Dictionary_2.hpp"
+#include "System/Action_1.hpp"
+
+#include "custom-types/shared/delegate.hpp"
+#include "bsml/shared/Helpers/delegates.hpp"
 
 using namespace GlobalNamespace;
 using namespace UnityEngine;
+
 // reimplementation so methods get called like shown
 MAKE_AUTO_HOOK_ORIG_MATCH(MultiplayerLobbyAvatarManager_AddPlayer, &MultiplayerLobbyAvatarManager::AddPlayer, void, MultiplayerLobbyAvatarManager* self, IConnectedPlayer* connectedPlayer) {
     if (connectedPlayer->get_isMe()) return;
@@ -43,20 +55,21 @@ MAKE_AUTO_HOOK_ORIG_MATCH(MultiplayerLobbyAvatarManager_AddPlayer, &MultiplayerL
     multiplayerLobbyAvatarController->ShowSpawnAnimation(playerWorldPosition, Quaternion::Euler(0, outerCirclePositionAngleForPlayer, 0));
 }
 
+// reimplementation so methods get called like shown
 MAKE_AUTO_HOOK_ORIG_MATCH(MultiplayerLobbyAvatarPlaceManager_SpawnAllPlaces, &MultiplayerLobbyAvatarPlaceManager::SpawnAllPlaces, void, MultiplayerLobbyAvatarPlaceManager* self) {
-    MultiplayerLobbyAvatarPlaceManager_SpawnAllPlaces(self);
+    self->DespawnAllPlaces();
 
-    // fix positions and rotations of the places
-    float angleBetweenPlayersWithEvenAdjustment = MultiplayerPlayerPlacement::GetAngleBetweenPlayersWithEvenAdjustment(self->lobbyStateDataModel->get_configuration().maxPlayerCount, MultiplayerPlayerLayout::Circle);
+    int maxPlayerCount = self->lobbyStateDataModel->get_configuration().maxPlayerCount;
+    float angleBetweenPlayersWithEvenAdjustment = MultiplayerPlayerPlacement::GetAngleBetweenPlayersWithEvenAdjustment(maxPlayerCount, MultiplayerPlayerLayout::Circle);
     float outerCircleRadius = std::max(MultiplayerPlayerPlacement::GetOuterCircleRadius(angleBetweenPlayersWithEvenAdjustment, self->innerCircleRadius), self->minOuterCircleRadius);
     int localPlayerSortIndex = self->lobbyStateDataModel->get_localPlayer()->get_sortIndex();
-    for (int idx = -1; auto multiplayerLobbyAvatarPlace : ListW<MultiplayerLobbyAvatarPlace*>(self->allPlaces)) {
-        idx++;
-
-        if (idx != localPlayerSortIndex) {
-            auto outerCirclePositionAngleForPlayer = MultiplayerPlayerPlacement::GetOuterCirclePositionAngleForPlayer(idx, localPlayerSortIndex, angleBetweenPlayersWithEvenAdjustment);
+    for (int i = 0; i < maxPlayerCount; i++) {
+        if (i != localPlayerSortIndex) {
+            auto outerCirclePositionAngleForPlayer = MultiplayerPlayerPlacement::GetOuterCirclePositionAngleForPlayer(i, localPlayerSortIndex, angleBetweenPlayersWithEvenAdjustment);
             auto playerWorldPosition = MultiplayerPlayerPlacement::GetPlayerWorldPosition(outerCircleRadius, outerCirclePositionAngleForPlayer, MultiplayerPlayerLayout::Circle);
+            auto multiplayerLobbyAvatarPlace = self->avatarPlacesPool->Spawn();
             multiplayerLobbyAvatarPlace->SetPositionAndRotation(playerWorldPosition, Quaternion::Euler(0, outerCirclePositionAngleForPlayer, 0));
+            self->allPlaces->Add(multiplayerLobbyAvatarPlace);
         }
     }
 }
@@ -67,31 +80,62 @@ MAKE_AUTO_HOOK_ORIG_MATCH(MultiplayerLobbyCenterStageManager_RecalculateCenterPo
 	self->centerObjectTransform->set_localPosition({0, 0, std::max(MultiplayerPlayerPlacement::GetOuterCircleRadius(angleBetweenPlayersWithEvenAdjustment, self->innerCircleRadius), self->minOuterCircleRadius)});
 }
 
+// reimplementation so methods get called like shown
 MAKE_AUTO_HOOK_ORIG_MATCH(MultiplayerPlayersManager_SpawnPlayers, &MultiplayerPlayersManager::SpawnPlayers, void, MultiplayerPlayersManager* self, MultiplayerPlayerStartState localPlayerStartState, System::Collections::Generic::IReadOnlyList_1<IConnectedPlayer*>* allActiveAtGameStartPlayers) {
-    MultiplayerPlayersManager_SpawnPlayers(self, localPlayerStartState, allActiveAtGameStartPlayers);
+    self->connectedPlayerControllersMap->Clear();
+    self->allActiveAtGameStartPlayers = allActiveAtGameStartPlayers;
+
     ListW<IConnectedPlayer*> allActiveAtGameStartPlayersW(self->allActiveAtGameStartPlayers);
+    auto localPlayer = self->multiplayerSessionManager->get_localPlayer();
+    auto layout = self->layoutProvider->CalculateLayout(allActiveAtGameStartPlayersW.size());
+    auto outerCircleRadius = self->beatmapObjectSpawnCenter->CalculateSpawnCenterPosition(allActiveAtGameStartPlayersW.size());
+
+    self->BindPlayerFactories(layout);
+
+    if (ConnectedPlayerHelpers::WasActiveAtLevelStart(localPlayer) && localPlayerStartState == MultiplayerPlayerStartState::InSync) {
+        self->activeLocalPlayerFacade = self->activeLocalPlayerFactory->Create(localPlayerStartState);
+        self->currentEventsPublisher = self->activeLocalPlayerFacade->i_IMultiplayerLevelEndActionsPublisher();
+        self->currentStartSeekSongControllerProvider = self->activeLocalPlayerFacade->i_IStartSeekSongControllerProvider();
+    } else {
+        self->inactiveLocalPlayerFacade = self->inactiveLocalPlayerFactory->Create(localPlayerStartState);
+        self->currentEventsPublisher = self->inactiveLocalPlayerFacade->i_IMultiplayerLevelEndActionsPublisher();
+        self->currentStartSeekSongControllerProvider = self->inactiveLocalPlayerFacade->i_IStartSeekSongControllerProvider();
+        self->inactiveLocalPlayerFacade->spectatorController->SwitchToDefaultSpot();
+        auto playerDidFinish = self->playerDidFinishEvent;
+        if (playerDidFinish) playerDidFinish->Invoke(nullptr);
+    }
+
+    self->currentEventsPublisher->add_playerDidFinishEvent(
+        BSML::MakeSystemAction<MultiplayerLevelCompletionResults*>(self, il2cpp_utils::il2cpp_type_check::MetadataGetter<&MultiplayerPlayersManager::HandlePlayerDidFinish>::get())
+    );
+
+    self->currentEventsPublisher->add_playerNetworkDidFailedEvent(
+        BSML::MakeSystemAction<MultiplayerLevelCompletionResults*>(self, il2cpp_utils::il2cpp_type_check::MetadataGetter<&MultiplayerPlayersManager::HandlePlayerNetworkDidFailed>::get())
+    );
 
     // if found, it's the index, if not found, it is end - begin == count
-    auto localPlayer = self->multiplayerSessionManager->get_localPlayer();
     int localPlayerIndex = std::find(allActiveAtGameStartPlayersW.begin(), allActiveAtGameStartPlayersW.end(), localPlayer) - allActiveAtGameStartPlayersW.begin();
+    if (!ConnectedPlayerHelpers::WasActiveAtLevelStart(localPlayer)) localPlayerIndex = allActiveAtGameStartPlayersW.size();
 
-    auto outerCircleRadius = self->beatmapObjectSpawnCenter->CalculateSpawnCenterPosition(allActiveAtGameStartPlayersW.size());
-    float angleBetweenPlayersWithEvenAdjustment = MultiplayerPlayerPlacement::GetAngleBetweenPlayersWithEvenAdjustment(self->allActiveAtGameStartPlayers->i_IReadOnlyCollection_1_T()->get_Count(), MultiplayerPlayerLayout::Circle);
-    auto layout = self->layoutProvider->CalculateLayout(allActiveAtGameStartPlayersW.size());
-    for (int idx = -1; auto connectedPlayer : allActiveAtGameStartPlayersW) {
-        idx++;
-        if (connectedPlayer->get_isMe()) continue;
+    float angleBetweenPlayersWithEvenAdjustment = MultiplayerPlayerPlacement::GetAngleBetweenPlayersWithEvenAdjustment(allActiveAtGameStartPlayersW.size(), layout);
+    for (int i = 0; i < allActiveAtGameStartPlayersW.size(); i++) {
+        auto connectedPlayer = allActiveAtGameStartPlayersW[i];
         auto userId = connectedPlayer->get_userId();
-        auto outerCirclePositionAngleForPlayer = MultiplayerPlayerPlacement::GetOuterCirclePositionAngleForPlayer(idx, localPlayerIndex, angleBetweenPlayersWithEvenAdjustment);
+        if (connectedPlayer->get_isMe()) {
+            self->connectedPlayerCenterFacingRotationsMap->set_Item(userId, 0.0f);
+            continue;
+        }
+
+        auto outerCirclePositionAngleForPlayer = MultiplayerPlayerPlacement::GetOuterCirclePositionAngleForPlayer(i, localPlayerIndex, angleBetweenPlayersWithEvenAdjustment);
         self->connectedPlayerCenterFacingRotationsMap->set_Item(userId, outerCirclePositionAngleForPlayer);
 
         if (!ConnectedPlayerHelpers::IsFailed(connectedPlayer) && ConnectedPlayerHelpers::WasActiveAtLevelStart(connectedPlayer)) {
             auto playerWorldPosition = MultiplayerPlayerPlacement::GetPlayerWorldPosition(outerCircleRadius, outerCirclePositionAngleForPlayer, layout);
-            auto multiplayerConnectedFacade = self->connectedPlayerControllersMap->get_Item(userId);
-
+            auto multiplayerConnectedFacade = self->connectedPlayerFactory->Create(connectedPlayer, localPlayerStartState);
             auto t = multiplayerConnectedFacade->get_transform();
             t->set_position(playerWorldPosition);
             t->set_rotation(Quaternion::Euler(0, outerCirclePositionAngleForPlayer, 0));
+            self->connectedPlayerControllersMap->set_Item(userId, multiplayerConnectedFacade);
         }
     }
 }
