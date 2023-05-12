f44278289d871f03c135c657b8f01cd234ac705d
EnderdracheLP <enderdrachelp@gmail.com>
2023-05-12 20:54:16 +0200

    Add try catch for deserialization

--- src/Networking/MpPacketSerializer.cpp
+++ src/Networking/MpPacketSerializer.cpp
@@ -1,4 +1,5 @@
 #include "Networking/MpPacketSerializer.hpp"
+#include "logging.hpp"
 
 DEFINE_TYPE(MultiplayerCore::Networking, MpPacketSerializer);
 
@@ -23,16 +24,29 @@ namespace MultiplayerCore::Networking {
     }
 
     void MpPacketSerializer::Deserialize(LiteNetLib::Utils::NetDataReader* reader, int length, GlobalNamespace::IConnectedPlayer* data) {
+        std::string packetId = "null";
         auto prevPosition = reader->get_Position();
-        std::string packetId(reader->GetString());
-        length -= reader->get_Position() - prevPosition;
-        prevPosition = reader->get_Position();
+        try {
+            packetId = static_cast<std::string>(reader->GetString());
+            length -= reader->get_Position() - prevPosition;
+            prevPosition = reader->get_Position();
 
-        auto handlerItr = packetHandlers.find(packetId);
-        if (handlerItr != packetHandlers.end() && handlerItr->second) {
-            handlerItr->second(reader, length, data);
+            auto handlerItr = packetHandlers.find(packetId);
+            if (handlerItr != packetHandlers.end() && handlerItr->second) {
+                handlerItr->second(reader, length, data);
+            }
         }
-
+        catch (const std::exception& e) {
+            std::string user;
+			if (data) {
+				if (data->get_userName())
+					user += static_cast<std::string>(data->get_userName());
+				if (!user.empty() && data->get_userId()) user += "|";
+				if (data->get_userId())
+					user += static_cast<std::string>(data->get_userId());
+			}
+            WARNING("An exception was thrown processing packet from player {}, packet {}: {}", user, packetId, e.what());
+        }
         int processedBytes = reader->get_Position() - prevPosition;
         reader->SkipBytes(length - processedBytes);
     }
