488c6e18675c73dc13d5a8c1b1e11b258b51aef6
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-05-15 20:36:34 +0200

    remove unneeded {}

--- shared/Networking/Abstractions/MpPacket.hpp
+++ shared/Networking/Abstractions/MpPacket.hpp
@@ -5,7 +5,7 @@
 #include "LiteNetLib/Utils/NetDataWriter.hpp"
 #include "LiteNetLib/Utils/INetSerializable.hpp"
 
-DECLARE_CLASS_CODEGEN_INTERFACES(MultiplayerCore::Networking::Abstractions, MpPacket, Il2CppObject, { classof(LiteNetLib::Utils::INetSerializable*) },
+DECLARE_CLASS_CODEGEN_INTERFACES(MultiplayerCore::Networking::Abstractions, MpPacket, Il2CppObject, classof(LiteNetLib::Utils::INetSerializable*),
 
     DECLARE_OVERRIDE_METHOD(void, Serialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Serialize>::get(), LiteNetLib::Utils::NetDataWriter* writer);
     DECLARE_OVERRIDE_METHOD(void, Deserialize, il2cpp_utils::il2cpp_type_check::MetadataGetter<&LiteNetLib::Utils::INetSerializable::Deserialize>::get(), LiteNetLib::Utils::NetDataReader* reader);
