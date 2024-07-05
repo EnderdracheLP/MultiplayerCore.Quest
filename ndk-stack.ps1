c5a7b5ea218f026407aba9051a61d8af816f138c
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-05 18:24:45 +0200

    Smaller changes for vscode and debugging

--- ndk-stack.ps1
+++ ndk-stack.ps1
@@ -1,7 +1,7 @@
 $NDKPath = $env:NdkPath
-if ($NDKPath -eq "") {
+# if ($NDKPath -eq "") {
     $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
-}
+# }
 
 $stackScript = "$NDKPath/ndk-stack"
 if (-not ($PSVersionTable.PSEdition -eq "Core")) {
@@ -9,7 +9,7 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 if ($args.Count -eq 0) {
-    Get-Content ./log.txt | & $stackScript -sym ./obj/local/arm64-v8a/ > log_unstripped.log
+    Get-Content ./log.txt | & $stackScript -sym ./build/ > log_unstripped.log
 } else {
-    Get-Content $args[0] | & $stackScript -sym ./obj/local/arm64-v8a/ > "$($args[0])_unstripped.txt"
+    Get-Content $args[0] | & $stackScript -sym ./build/ > "$($args[0])_unstripped.txt"
 }
