f1732f07c39d4364b425c1ac8aa5371b0aa45b02
EnderdracheLP <enderdrachelp@elp.quest>
2025-07-16 21:09:21 +0200

    Make bools atomic (thread-safe), add more debug loggers, use simpler check instead of std::search

--- src/Hooks/PlatformAuthenticationTokenProviderHooks.cpp
+++ src/Hooks/PlatformAuthenticationTokenProviderHooks.cpp
@@ -19,17 +19,18 @@
 
 #include "custom-types/shared/delegate.hpp"
 
+#include <atomic>
 #include <sys/system_properties.h>
 
 static ConstString dummy_auth("who_is_rem_?");
 
-static std::string PicoBrand = "PICO";
-static bool isPico = false;
-static bool checkedForPico = false;
+static std::string PicoBrand = "pico";
+static std::atomic_bool isPico;
+static std::atomic_bool checkedForPico;
 
 MAKE_AUTO_HOOK_ORIG_MATCH(PlatformAuthenticationTokenProvider_GetAuthenticationToken, &GlobalNamespace::PlatformAuthenticationTokenProvider::GetAuthenticationToken, 
 System::Threading::Tasks::Task_1<GlobalNamespace::AuthenticationToken>*, GlobalNamespace::PlatformAuthenticationTokenProvider* self) {
-    if (isPico) self->_platform = GlobalNamespace::AuthenticationToken::Platform(20); // Set platform to 20 for Pico
+    if (isPico.load()) self->_platform = GlobalNamespace::AuthenticationToken::Platform(20); // Set platform to 20 for Pico
 
     auto t = PlatformAuthenticationTokenProvider_GetAuthenticationToken(self);
     return MultiplayerCore::StartTask<GlobalNamespace::AuthenticationToken>([=](){
@@ -85,31 +86,43 @@ System::Threading::Tasks::Task_1<GlobalNamespace::AuthenticationToken>*, GlobalN
 
 
 MAKE_AUTO_HOOK_ORIG_MATCH(OculusPlatformUserModel_GetUserInfo, &GlobalNamespace::OculusPlatformUserModel::GetUserInfo, System::Threading::Tasks::Task_1<GlobalNamespace::UserInfo*>*, GlobalNamespace::OculusPlatformUserModel* self, System::Threading::CancellationToken ctx) {;
-    if (!checkedForPico) {
+    if (!checkedForPico.load()) {
         auto prop = __system_property_find("ro.product.brand");
         if (prop)
             __system_property_read_callback(prop, [](void* cookie, const char* name, const char* value, uint32_t serial) {
                 std::string brand(value);
                 INFO("Device brand: {}", brand);
-                isPico = std::search(brand.begin(), brand.end(), PicoBrand.begin(), PicoBrand.end(), [](char a, char b){
+                // std::string brand_lower;
+                // brand_lower.resize(brand.size());
+                // std::transform(brand.begin(), brand.end(), brand_lower.begin(),
+                //             [](unsigned char c){ return std::tolower(c); });
+                // isPico.store(brand_lower.find(PicoBrand) != std::string::npos);
+                isPico.store(std::search(brand.begin(), brand.end(), PicoBrand.begin(), PicoBrand.end(), [](char a, char b){
                     return std::tolower(static_cast<unsigned char>(a)) == std::tolower(static_cast<unsigned char>(b));
-                }) != brand.end();
-                checkedForPico = true;
+                }) != brand.end());
+                DEBUG("Brand Check result: {}", isPico.load());
+                checkedForPico.store(true);
             }, nullptr);
     }
 
+    DEBUG("Running Orig");
     auto t = OculusPlatformUserModel_GetUserInfo(self, ctx);
+    DEBUG("Got Orig task, starting custom task");
     return MultiplayerCore::StartTask<GlobalNamespace::UserInfo*>([=](){
         using namespace std::chrono_literals;
+        DEBUG("Start UserInfoTask");
         while (!(t->IsCompleted || t->IsCanceled)) std::this_thread::sleep_for(50ms);
         GlobalNamespace::UserInfo* info = t->ResultOnSuccess;
+        DEBUG("Task finished getting result");
         if (info) {
             INFO("Successfully got user info, returning it!");
-            if (isPico)
+            if (isPico.load())
             {
                 INFO("User is on Pico, changing platform to 20");
                 info->platform = GlobalNamespace::UserInfo::Platform(20);
             }
+        } else {
+            ERROR("UserInfo null!");
         }
         return info;
     });
